65e29b139da3f2d63b2276983d3266643f805e23
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2024-12-11 18:21:34 +0500

    delete commented code

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -1,27 +1,3 @@
-// "use client";
-
-// import { useEffect, useState } from "react";
-
-// export default function CountryLookup() {
-//   const [country, setCountry] = useState("USA");
-
-//   useEffect(() => {
-//     const getCounrty = async () => {
-//       const response = await fetch(
-//         `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
-//       )
-//         .then((res) => res.json())
-//         .then((res) => DataTransfer.country);
-//       console.log(response);
-//       if (!response) return;
-//       setCountry(response);
-//     };
-//     getCounrty();
-//   }, []);
-
-//   return <div>{country}</div>;
-// }
-
 "use client";
 
 import { useEffect, useState } from "react";
